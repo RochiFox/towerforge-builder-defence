4d8fcb57388e36c323288ca7a6c0806cc33b5075
Rochi <rochithefox@gmail.com>
2025-06-21 15:28:26 +0300

    Added: place building

---
+++ Towerforge Builder Defence/Assets/Scripts/BuildingManager.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class BuildingManager : MonoBehaviour
+{
+    [SerializeField] private Transform harvesterPrefab;
+
+    private Camera mainCamera;
+
+    private void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            Instantiate(harvesterPrefab, GetMouseWorldPosition(), Quaternion.identity);
+    }
+
+    private Vector3 GetMouseWorldPosition()
+    {
+        Vector3 mouseWorldPosition = mainCamera.ScreenToWorldPoint(Input.mousePosition);
+        mouseWorldPosition.z = 0f;
+
+        return mouseWorldPosition;
+    }
+}
